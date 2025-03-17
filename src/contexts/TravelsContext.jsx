c1a40ec89a8318cba6d84c7f188c813957a3ecda
penso996 <penso.andrea@outlook.it>
2025-03-17 18:32:05 +0100

    added main stuff

---
+++ src/contexts/TravelsContext.jsx
@@ -0,0 +1,18 @@
+const travels = [
+    {
+        id: 1,
+        destination: "Roma",
+        startDate: "2024-05-10",
+        endDate: "2024-05-15",
+        travelers: [
+            {
+                id: 1,
+                name: "Mario",
+                surname: "Rossi",
+                email: "mario.rossi@mail.it",
+                phone: "1234567890",
+                fiscalCode: "MRORSS80A01H501U"
+            },
+        ]
+    }
+]
